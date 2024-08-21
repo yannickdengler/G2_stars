ba2bf6cc99edec5da0fe9803293104bd0055441e
Yannick Dengler <yannick.dengler@uni-graz.at>
2024-08-21 18:41:31 +0200

    Plots look very good

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -12,14 +12,20 @@ EoS = int(sys.argv[1])
 EoS_str = ""
 if EoS == 1:
     EoS_str = "I"
-    def EoS_max(m_DM):
+    def EoS_max(m_DM,dm_om):
+        if dm_om == -1:
+            return 1e50
         return 582.1/m_DM**4*MeV_fm**3
 elif EoS == 2:
-    def EoS_max(m_DM):
+    def EoS_max(m_DM,dm_om):
+        if dm_om == -1:
+            return 1e50
         return 238.7/m_DM**4*MeV_fm**3
     EoS_str = "II"
 elif EoS == 3:
-    def EoS_max(m_DM):
+    def EoS_max(m_DM,dm_om):
+        if dm_om == -1:
+            return 1e50
         return 136.8/m_DM**4*MeV_fm**3
     EoS_str = "III"
 else:
@@ -49,11 +55,11 @@ def get_stable(EoS_str, dm_om, M_DM, norm=1):
     tmp = np.genfromtxt("results/full_EoS_%s_run/stability_P_dm_om_%1.3e_M_%1.3e_2_fluid"%(EoS_str,dm_om, M_DM))[1:]
     stability = []
     for k in range(len(tmp)):
-        if (tmp[k][0] < EoS_max(M_DM)):
-            if (tmp[k][1] <= EoS_max(M_DM)):
+        if (tmp[k][0] < EoS_max(M_DM,dm_om)):
+            if (tmp[k][1] <= EoS_max(M_DM,dm_om)):
                 stability.append(list(tmp[k]))
             else:
-                stability.append([tmp[k][0],EoS_max(M_DM)])
+                stability.append([tmp[k][0],EoS_max(M_DM,dm_om)])
     M_R_stable = []
     for m in range(len(stability)):               # num of stable in branch
         M_R_stable.append([])
@@ -90,16 +96,16 @@ def combine_stables(M_R_stable):
                 res[m].append(M_R_stable[k][m][l])
     return res
 
-def combine_stables_dm_om_M_DM(M_R_stable):
-    res = []
-    for i in range(len(M_R_stable)):
-        res.append([])
-        for j in range(len(M_R_stable[i])):
-            res[i].append([])
-            combine = combine_stables(M_R_stable[i][j])
-            for k in range(len(combine)):
-                res[i][j].append(combine[k])
-    return res
+# def combine_stables_dm_om_M_DM(M_R_stable):
+#     res = []
+#     for i in range(len(M_R_stable)):
+#         res.append([])
+#         for j in range(len(M_R_stable[i])):
+#             res[i].append([])
+#             combine = combine_stables(M_R_stable[i][j])
+#             for k in range(len(combine)):
+#                 res[i][j].append(combine[k])
+#     return res
 
 def get_data(M_R_stable):
     """
@@ -116,11 +122,13 @@ def get_data(M_R_stable):
     res["R_DM"]=M_R_stable[2]
     res["R_max"]=[max(M_R_stable[1][i],M_R_stable[2][i]) for i in range(len(M_R_stable[3]))]
     res["M_max"]=res["M_sum"][ind_crit]
-    res["M_max_OM"]=M_R_stable[3][ind_crit]
-    res["M_max_DM"]=M_R_stable[4][ind_crit]
+    res["M_crit_OM"]=M_R_stable[3][ind_crit]
+    res["M_crit_DM"]=M_R_stable[4][ind_crit]
     res["R_crit"]=res["R_max"][ind_crit]
     res["R_crit_OM"]=M_R_stable[1][ind_crit]
     res["R_crit_DM"]=M_R_stable[2][ind_crit]
+    res["k2"]=M_R_stable[7]
+    res["C"]=M_R_stable[6]
     res["k2_max"]=max(M_R_stable[7])
     res["k2_crit"]=M_R_stable[7][ind_crit]
     res["C_max"]=max(M_R_stable[6])
@@ -146,9 +154,9 @@ def get_data_dm_om_M_arr(M_R_stable):
                 res[key][i][j] = data[i][j][key]
     return res
 
-def plot_3D(arr, label):
+def plot_3D(arr, label, save=False, show=False):
     '''
-        Creates a 3D plot in x-m_DM, y-dm_om, z-"arr" (with label=label)
+        Creates a 3D plot in x-m_DM, y-dm_om, z-"arr" (with label=label), arr as [dm_om][m_DM]
     '''
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
@@ -156,14 +164,19 @@ def plot_3D(arr, label):
 
     surf = ax.plot_surface(np.log10(M_plot), np.log10(dm_om_plot), np.log10(arr), cmap=cm.coolwarm, linewidth=0, antialiased=False)
 
-    ax.set_zlabel(label)
+    # ax.set_zlabel(label)
     ax.set_xlabel("$M_{DM}$")
-    ax.set_ylabel("$p_{0,DM}/p_{0,OM}")
+    ax.set_ylabel("$p_{0,DM}/p_{0,OM}$")
     ax.zaxis.set_major_locator(LinearLocator(10))
     ax.zaxis.set_major_formatter('{x:.02f}')
 
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    plt.show()
+    fig.colorbar(surf, shrink=0.5, aspect=5, label=label)
+    if save:
+        plt.savefig("plots/full_EoS_%s_run/%s_3D_EoS_%s.pdf"%(EoS_str,label,EoS_str))
+    if show:
+        plt.show()
+    ax.cla()
+    plt.close(fig)
 
 def plot_M_R_dm_om(data, M_ind, show=True, save = True):
     cmap = mpl.colormaps['viridis']
@@ -175,67 +188,72 @@ def plot_M_R_dm_om(data, M_ind, show=True, save = True):
         plt.savefig("plots/full_EoS_%s_run/M_R_EoS_%s_dm_om_M_DM_%e.pdf"%(EoS_str,EoS_str,M_arr[M_ind]))
     if show:
         plt.show()
-    # plt.clf()
 
-def plot_M_R_dm_om_keep_stable(data, M_ind, show=True, save = True):
-    cmap = mpl.colormaps['viridis']
-    colors = cmap(np.linspace(0, 1, 15))
+def plot_M_R_dm_om_keep_stable(data, data_OM, data_DM, M_ind, show=True, save = True):
+    fig, ax = plt.subplots()
+    norm = mpl.colors.Normalize(vmin=np.log10(min(dm_om_arr)),vmax=np.log10(max(dm_om_arr)))
+    cmap = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.viridis)
+    cmap.set_array([])
+    colors = cmap.to_rgba(np.linspace(np.log10(min(dm_om_arr)), np.log10(max(dm_om_arr)), len(dm_om_arr)))
     plt.grid()
+    R_max = 0
+    M_max = 0
     for i in range(len(data)):
         for j in range(len(data[i][M_ind])):
+            R_max = max([R_max, max(data[i][M_ind][j]["R_max"])])
+            M_max = max([M_max, max(data[i][M_ind][j]["M_sum"])])
             plt.plot(data[i][M_ind][j]["R_max"], data[i][M_ind][j]["M_sum"], color = colors[i])
+    ax.plot(data_OM["R_max"], data_OM["M_sum"], color = "black", label = "OM", ls = "dashed")
+    ax.plot(data_DM["R_max"], data_DM["M_sum"], color = "black", label = "DM", ls = "solid")
+    ax.set_xlabel("$R_{max}$")
+    ax.set_ylabel("$M_{tot}$")
+    ax.set_xlim([0,1.1*R_max])
+    ax.set_ylim([0,1.1*M_max])
+    cb = plt.colorbar(cmap)
+    cb.set_label("$p_{0,DM}/p_{0,OM}$")
+    plt.title("$m_{DM}=%1.1e MeV$"%M_arr[M_ind])
+    plt.legend()
     if save:
         plt.savefig("plots/full_EoS_%s_run/M_R_EoS_%s_dm_om_M_DM_%e.pdf"%(EoS_str,EoS_str,M_arr[M_ind]))
     if show:
         plt.show()
-
-# def plot_M_R_dm_om_branches(data, M_ind, show=True, save = True):
-#     cmap = mpl.colormaps['viridis']
-#     colors = cmap(np.linspace(0, 1, len(data["R_max"])))
-#     plt.grid()
-#     for i in range(len(data["R_max"])):
-#         plt.plot(data["R_max"][i][M_ind], data["M_sum"][i][M_ind], color = colors[i])
-#     if save:
-#         plt.savefig("plots/full_EoS_%s_run/M_R_EoS_%s_dm_om_M_DM_%e.pdf"%(EoS_str,EoS_str,M_arr[M_ind]))
-#     if show:
-#         plt.show()
-    # plt.clf()
-
-# def plot_M_R_M_DM(data, dm_om_ind, show=True, save = True):
-#     cmap = mpl.colormaps['viridis']
-#     colors = cmap(np.linspace(0, 1, len(data[0][0][0]["R_max"])))
-#     plt.grid()
-#     for dat in data:
-#         for i in range(len(dat[0][0]["R_max"])):
-#             plt.plot(dat[dm_om_ind][i]["R_max"], dat[dm_om_ind][i]["M_sum"], color = colors[i])
-#     if save:
-#         plt.savefig("plots/full_EoS_%s_run/M_R_EoS_%s_M_DM_dm_om_%e.pdf"%(EoS_str,EoS_str,dm_om_arr[dm_om_ind]))
-#     if show:
-#         plt.show()
-#     plt.clf()
-
-# def plot_M_R_M_DM_branches(data, dm_om_ind, show=True, save = True):
-#     cmap = mpl.colormaps['viridis']
-#     colors = cmap(np.linspace(0, 1, len(data["R_max"])))
-#     plt.grid()
-#     for dat in data:
-#         for i in range(len(data["R_max"][0])):
-#             plt.plot(dat["R_max"][dm_om_ind][i], dat["M_sum"][dm_om_ind][i], color = colors[i])
-#     if save:
-#         plt.savefig("plots/full_EoS_%s_run/M_R_EoS_%s_M_DM_dm_om_%e.pdf"%(EoS_str,EoS_str,dm_om_arr[dm_om_ind]))
-#     if show:
-#         plt.show()
-#     plt.clf()
+    ax.cla()
+    plt.close(fig)
 
 
-if __name__ == "__main__":
+def plot_k_2_C_dm_om_keep_stable(data, data_OM, data_DM, M_ind, show=True, save = True):
+    fig, ax = plt.subplots()
+    norm = mpl.colors.Normalize(vmin=np.log10(min(dm_om_arr)),vmax=np.log10(max(dm_om_arr)))
+    cmap = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.viridis)
+    cmap.set_array([])
+    colors = cmap.to_rgba(np.linspace(np.log10(min(dm_om_arr)), np.log10(max(dm_om_arr)), len(dm_om_arr)))
+    plt.grid()
+    x_max = 0
+    y_max = 0
+    for i in range(len(data)):
+        for j in range(len(data[i][M_ind])):
+            x_max = max([x_max, max(data[i][M_ind][j]["C"])])
+            y_max = max([y_max, max(data[i][M_ind][j]["k2"])])
+            plt.plot(data[i][M_ind][j]["C"], data[i][M_ind][j]["k2"], color = colors[i])
+    ax.plot(data_OM["C"], data_OM["k2"], color = "black", label = "OM", ls = "dashed")
+    ax.plot(data_DM["C"], data_DM["k2"], color = "black", label = "DM", ls = "solid")
+    ax.set_xlabel("C")
+    ax.set_ylabel("$k2$")
+    ax.set_xlim([0,1.1*x_max])
+    ax.set_ylim([0,1.1*y_max])
+    cb = plt.colorbar(cmap)
+    cb.set_label("$p_{0,DM}/p_{0,OM}$")
+    plt.title("$m_{DM}=%1.1e MeV$"%M_arr[M_ind])
+    plt.legend()
+    if save:
+        plt.savefig("plots/full_EoS_%s_run/k_2_C_EoS_%s_dm_om_M_DM_%e.pdf"%(EoS_str,EoS_str,M_arr[M_ind]))
+    if show:
+        plt.show()
+    ax.cla()
+    plt.close(fig)
+
+def get_data_stable():
     stable = get_stable_M_R_dm_om_M_arr()
-    # stable_c = combine_stables_dm_om_M_DM(stable)
-    # data = get_data_dm_om_M_arr(stable_c)
-
-    # data = []
-    # for k in range(len(stable[0][0])):
-    #     data.append(get_data_dm_om_M_arr([[stable[j][i][k] for i in range(len(stable[0]))] for j in range(len(stable))]))
 
     data = []
     for i in range(len(stable)):             # dm_om
@@ -244,22 +262,71 @@ if __name__ == "__main__":
             data[i].append([])
             for k in range(len(stable[i][j])):  # stable
                 data[i][j].append(get_data(stable[i][j][k]))
+    return data
 
+def get_data_stable_OM_DM(norm):
+    stable_pure_OM, stable_pure_DM = get_stable_M_R_pure(norm=norm)
+    stable_c_pure_OM = combine_stables(stable_pure_OM)
+    data_OM = get_data(stable_c_pure_OM)
+    stable_c_pure_DM = combine_stables(stable_pure_DM)
+    data_DM = get_data(stable_c_pure_DM)
+    return data_OM, data_DM
+
+def plot_all_M_tot_R_tot():
+    data = get_data_stable()
     for i in range(15):
-        stable_pure_OM, stable_pure_DM = get_stable_M_R_pure(norm=(M_arr[i]/1000)**(-2))
-        stable_c_pure_OM = combine_stables(stable_pure_OM)
-        data_OM = get_data(stable_c_pure_OM)
-        stable_c_pure_DM = combine_stables(stable_pure_DM)
-        data_DM = get_data(stable_c_pure_DM)
+        data_OM, data_DM = get_data_stable_OM_DM((M_arr[i]/1000)**(-2))
+        plot_M_R_dm_om_keep_stable(data,data_OM,data_DM, i, show = False, save=True)
 
-        plt.plot(data_OM["R_max"], data_OM["M_sum"], color = "black", label = "OM", ls = "dashed")
-        plt.plot(data_DM["R_max"], data_DM["M_sum"], color = "black", label = "DM", ls = "dashdot")
-        plt.legend()
-        # for dat in data:
-        # plot_M_R_dm_om(data, i, show = False, save=False)
-        plot_M_R_dm_om_keep_stable(data, i, show = False, save=False)
-        plt.savefig("plots/full_EoS_%s_run/M_R_EoS_%s_dm_om_M_DM_%e.pdf"%(EoS_str,EoS_str,M_arr[i]))
-        plt.clf()
+def plot_all_k_2_C():
+    data = get_data_stable()
+    for i in range(15):
+        data_OM, data_DM = get_data_stable_OM_DM((M_arr[i]/1000)**(-2))
+        plot_k_2_C_dm_om_keep_stable(data,data_OM,data_DM, i, show = False, save=True)
+
+def get_arr(data, label, stab_max_inds):
+    tmp = [[[data[i][j][k][label] for k in range(len(data[i][j]))] for j in range(len(M_arr))] for i in range(len(dm_om_arr))]
+
+    arr = []
+    for i in range(len(dm_om_arr)):
+        arr.append([])
+        for j in range(len(M_arr)):
+            arr[i].append(tmp[i][j][stab_max_inds[i][j]])
+    return arr
+
+def plot_all_3D():
+    data = get_data_stable()
+
+    tmp = [[[data[i][j][k]["M_max"] for k in range(len(data[i][j]))] for j in range(len(M_arr))] for i in range(len(dm_om_arr))]
+
+    stab_max_inds = []
+    M_max_arr = []
+    for i in range(len(dm_om_arr)):
+        stab_max_inds.append([])
+        M_max_arr.append([])
+        for j in range(len(M_arr)):
+            stab_max_inds[i].append(np.argmax(tmp[i][j]))
+            M_max_arr[i].append(tmp[i][j][stab_max_inds[i][j]])
+    
+    plot_3D(M_max_arr, label="$M_{max}$", save=True, show=False)
+    for key in ["R_crit", "k2_crit", "C_crit", "M_crit_OM", "M_crit_DM", "R_crit_OM", "R_crit_DM"]:
+        plot_3D(get_arr(data, key, stab_max_inds), key, save=True, show=False)
+        
+    tmp = [[[data[i][j][k]["k2_max"] for k in range(len(data[i][j]))] for j in range(len(M_arr))] for i in range(len(dm_om_arr))]
+    k2_max_arr = []
+    for i in range(len(dm_om_arr)):
+        k2_max_arr.append([])
+        for j in range(len(M_arr)):
+            k2_max_arr[i].append(max(tmp[i][j]))
+    plot_3D(k2_max_arr, label="$k_{2max}$", save=True, show=True)
+    
+
+
+
+if __name__ == "__main__":
+    # plot_all_M_tot_R_tot()
+    # plot_all_k_2_C()
+    plot_all_3D()
 
         
 
@@ -267,113 +334,3 @@ if __name__ == "__main__":
 
 
 
-
-
-    # plot_3D(data["M_max"],"M_max")
-
-    ##########################################################
-
-
-
-
-# def get_data(M_R_stable):
-#     res = {}
-#     res["M_OM"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["M_DM"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["M_sum"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["ind_crit"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["p_crit"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["R_OM"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["R_DM"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["R_max"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["M_max"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["M_max_OM"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["M_max_DM"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["R_crit"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["R_crit_OM"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["R_crit_DM"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["k2_max"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["k2_crit"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["C_max"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-#     res["C_crit"]=[[[] for i in range(len(M_R_stable))] for j in range(len(M_R_stable[0]))]
-
-#     for i in range(len(M_R_stable)):
-#         for j in range(len(M_R_stable[i])):
-#             print(i,j)
-#             res["M_OM"][i][j] = M_R_stable[i][j][3]
-#             res["M_DM"][i][j] = M_R_stable[i][j][4]
-#             for k in range(len(M_R_stable[i][j][3])):
-#                 res["M_sum"][i][j].append(M_R_stable[i][j][3][k]+M_R_stable[i][j][4][k])
-#             ind_crit = np.argmax(res["M_sum"][i][j])
-#             res["ind_crit"][i][j] = ind_crit
-#             res["R_OM"][i][j] = M_R_stable[i][j][1]
-#             res["R_DM"][i][j] = M_R_stable[i][j][2]
-#             res["R_max"][i][j]=max(M_R_stable[i][j][1],M_R_stable[i][j][2])
-#             res["M_max"][i][j] = res["M_sum"][i][j][ind_crit]
-#             res["M_max_OM"][i][j] = M_R_stable[i][j][3][ind_crit]
-#             res["M_max_DM"][i][j] = M_R_stable[i][j][4][ind_crit]
-#             res["R_crit"][i][j] = res["R_max"][i][j][ind_crit]
-#             res["R_crit_OM"][i][j] =  M_R_stable[i][j][1][ind_crit]
-#             res["R_crit_DM"][i][j] =  M_R_stable[i][j][2][ind_crit]
-#             res["k2_max"][i][j] = max(M_R_stable[i][j][7])
-#             res["k2_crit"][i][j] = M_R_stable[i][j][7][ind_crit]
-#             res["C_max"][i][j] = max(M_R_stable[i][j][6])
-#             res["C_crit"][i][j] = M_R_stable[i][j][6][ind_crit]
-#             res["p_crit"][i][j] = M_R_stable[i][j][0][ind_crit]
-#     return res
-
-
-
-# def combine_stables(M_R_stable):
-#     '''
-#         return an array which connects the stable branches of M_R_stable to give a shape of [dm_om][m_DM][ops][p0].
-#     '''
-#     res = []
-#     for i in range(len(M_R_stable)):
-#         res.append([])
-#         for j in range(len(M_R_stable[i])):
-#             res[i].append([])
-#             for k in range(len(M_R_stable[i][j])):                  # stable
-#                 for m in range(len(M_R_stable[i][j][k])):           # obs
-#                     res[i][j].append([])
-#                     for l in range(len(M_R_stable[i][j][k][m])):             # p0
-#                         res[i][j][m].append(M_R_stable[i][j][k][m][l])
-#     return res
-
-
-
-
-# def get_stable_M_R_dm_om_M_arr():
-#     '''
-#         returns an array for the stable branches of all M_DM and dm_om of shape [dm_om][m_DM][stability][ops][p0].
-#     '''
-#     M_R_stable = []
-#     for i in range(len(dm_om_arr)):
-#         M_R_stable.append([])
-#         for j in range(len(M_arr)):
-#             M_R_stable[i].append([])
-#             mass_DM = M_arr[j]
-#             con_mass = pow(Planck_Mass, 3)*pow(mass_DM*MeV_solar, -2)
-#             con_radius = pow(Planck_Mass, 2)*pow(mass_DM*MeV_solar, -2)*Planck_Length               #1.769 * 1e-76# *neutron_mass_fm**2
-#             M_R = np.genfromtxt("results/full_EoS_%s_run/M_R_TIDAL_P_dm_om_%1.3e_M_%1.3e_2_fluid.out"%(EoS_str,dm_om_arr[i], M_arr[j]))
-#             for ii in range(len(M_R)):
-#                 M_R[ii][1] = con_radius*M_R[ii][1]
-#                 M_R[ii][2] = con_radius*M_R[ii][2]
-#                 M_R[ii][3] = con_mass*M_R[ii][3]
-#                 M_R[ii][4] = con_mass*M_R[ii][4]
-#             tmp = np.genfromtxt("results/full_EoS_%s_run/stability_P_dm_om_%1.3e_M_%1.3e_2_fluid"%(EoS_str,dm_om_arr[i], M_arr[j]))[1:]
-#             stability = []
-#             for k in range(len(tmp)):
-#                 if (tmp[k][0] < EoS_max(mass_DM)):
-#                     if (tmp[k][1] <= EoS_max(mass_DM)):
-#                         stability.append(list(tmp[k]))
-#                     else:
-#                         stability.append([tmp[k][0],EoS_max(mass_DM)])
-#             for m in range(len(stability)):               # num of stable in branch
-#                 M_R_stable[i][j].append([])
-#                 for k in range(len(M_R[0])):              # num of obs  
-#                     M_R_stable[i][j][m].append([])
-#                     for l in range(len(M_R)):             # num of P0
-#                         if M_R[l][0] > stability[m][0] and M_R[l][0] < stability[m][1]:
-#                             M_R_stable[i][j][m][k].append(M_R[l][k])
-#     return M_R_stable
